baf17aee491ba5eed128221255f118f85867b39b
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-12-03 16:31:32 +0100

    tests refactoring

--- src/test/kotlin/mobi/hsz/adventofcode/Day0Test.kt
+++ src/test/kotlin/mobi/hsz/adventofcode/Day0Test.kt
@@ -1,29 +1,28 @@
 package mobi.hsz.adventofcode
 
 import org.junit.jupiter.api.DisplayName
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.api.TestFactory
 
 @DisplayName("Advent of Code 2019, Day 0:")
-class Day0Test {
+class Day0Test : DayTest(Day0()) {
 
-    @Test
+    @TestFactory
     @DisplayName("Part 1")
-    fun part1() {
-        val day = Day0()
+    fun part1() = test(
+        function = day::part1,
+        answer = 0,
+        data = listOf(
 
-        assertEquals(day.part1(""), 0)
+        )
+    )
 
-        assertEquals(day.part1(), 0)
-    }
-
-    @Test
+    @TestFactory
     @DisplayName("Part 2")
-    fun part2() {
-        val day = Day0()
+    fun part2() = test(
+        function = day::part2,
+        answer = 0,
+        data = listOf(
 
-        assertEquals(day.part2(""), 0)
-
-        assertEquals(day.part2(), 0)
-    }
+        )
+    )
 }
